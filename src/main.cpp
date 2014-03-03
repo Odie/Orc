d24fd255fab02b30901e1b9d9f67a14eaff48e60
Jonathan Shieh <jonathan.shieh@gmail.com>
2014-03-03 16:56:34 +0800

    Rendering our first triangle to the screen!

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,79 @@ void logMessage(const char* format, ...)
 	va_end(argList);
 }
 
+GLuint loadShader(GLenum shaderType, const char* filePath)
+{
+	GLuint shaderID = glCreateShader(shaderType);
+
+	// read the entire shader file
+	std::ifstream shaderFile(filePath);
+	if(!shaderFile.is_open()){
+		logError("[Shader] Couldn't open file:  %s\n", filePath);
+		return 0;
+	}
+
+	std::stringstream buffer;
+	buffer << shaderFile.rdbuf();
+
+	const char* shaderContents = buffer.str().c_str();
+
+
+	// compile the shader
+	logMessage("[Shader] Compiling:  %s\n", filePath);
+	glShaderSource(shaderID, 1, &shaderContents , NULL);
+	glCompileShader(shaderID);
+
+	// report any shader compiliation errors
+	GLint result = GL_FALSE;
+	int infoLogLength;
+
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+	if (infoLogLength > 0){
+		std::vector<char> errorMessage(infoLogLength + 1);
+		glGetShaderInfoLog(shaderID, infoLogLength, NULL, &errorMessage[0]);
+		logError("[Shader] Compile error: %s", &errorMessage[0]);
+		return 0;
+	}
+
+	// return the id of the compiled shader
+	return shaderID;
+}
+
+GLuint loadShaderProgram(const char * vertexFilePath,const char * fragmentFilePath){
+
+	// load the shaders
+	GLuint vertShaderID = loadShader(GL_VERTEX_SHADER, vertexFilePath);
+	GLuint fragShaderID = loadShader(GL_FRAGMENT_SHADER, fragmentFilePath);
+	if(vertShaderID == 0 || fragShaderID == 0)
+		return 0;
+
+	// Link the program
+	logMessage("[Shader] linking program\n");
+
+	GLuint programID = glCreateProgram();
+	glAttachShader(programID, vertShaderID);
+	glAttachShader(programID, fragShaderID);
+	glLinkProgram(programID);
+
+	// report any program link errors
+	GLint result = GL_FALSE;
+	int infoLogLength;
+
+	glGetProgramiv(programID, GL_LINK_STATUS, &result);
+	glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+	if (infoLogLength > 0){
+		std::vector<char> errorMessage(infoLogLength + 1);
+		glGetProgramInfoLog(programID, infoLogLength, NULL, &errorMessage[0]);
+		logError("[Shader] Link error: %s", &errorMessage[0]);
+	}
+
+	glDeleteShader(vertShaderID);
+	glDeleteShader(fragShaderID);
+
+	return programID;
+}
+
 void handleKeystrokes()
 {
 	SDL_Event event;
@@ -44,6 +117,8 @@ void handleKeystrokes()
 	while(SDL_PollEvent(&event)){
 
 		switch(event.type){
+			/* Keyboard event */
+			/* Pass the event data onto PrintKeyInfo() */
 			case SDL_KEYDOWN:
 				if(event.key.keysym.sym == SDLK_ESCAPE)
 					exit(0);
@@ -62,14 +137,56 @@ void handleKeystrokes()
 
 void engineLoop()
 {
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// Load the shader
+	GLuint programID = loadShaderProgram("assets/basic.vertexshader", "assets/basic.fragmentshader");
+
+	// Setup the model to display
+	GLuint vertexArrayID;
+	glGenVertexArrays(1, &vertexArrayID);
+	glBindVertexArray(vertexArrayID);
+
+	glUseProgram(programID);
+
+	static const GLfloat vertexData[] = {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+	glBindVertexArray(0);
 
 	while(true){
 		handleKeystrokes();
 
 		// Clear the screen
-		glClear( GL_COLOR_BUFFER_BIT );
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(vertexArrayID);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		// Swap buffers
+		SDL_GL_SwapWindow(globals.window);
 	}
+
+	// TODO!!!
+	// Cleanup programID and vertexArrayID
 }
 
 void shutdown()
